b95b5b06fa61bc655fc300acc5d0aec70a157fb0
imostafiz <mostafiz1257@gmail.com>
2026-09-03 15:55:39 +0600

    Revert "check git revert"
    This reverts commit 6c5ec7bde3efd7751ef64526c9200f425b8f738c.

--- src/components/shared/sidebar/LeftSidebar.tsx
+++ src/components/shared/sidebar/LeftSidebar.tsx
@@ -104,10 +104,7 @@ const LeftSidebar = () => {
               {" "}
               Find Me
             </p>
-            <p className="text-3xl font-title2 font-bold text-white">
-              {" "}
-              Postly from v3
-            </p>
+            <p className="text-3xl font-title2 font-bold text-white"> Postly</p>
           </li>
           {user?.isPremium === false && (
             <li>
