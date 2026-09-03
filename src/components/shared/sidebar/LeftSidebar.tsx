9a5026a89f2ce644bb8e39176e9d727af75d1c89
imostafiz <mostafiz1257@gmail.com>
2026-09-03 15:02:38 +0600

    git Rebase check

--- src/components/shared/sidebar/LeftSidebar.tsx
+++ src/components/shared/sidebar/LeftSidebar.tsx
@@ -100,6 +100,10 @@ const LeftSidebar = () => {
       >
         <ul className="text-xl text-white">
           <li className="">
+            <p className="text-3xl font-title2 font-bold text-white">
+              {" "}
+              Find Me
+            </p>
             <p className="text-3xl font-title2 font-bold text-white"> Postly</p>
           </li>
           {user?.isPremium === false && (
